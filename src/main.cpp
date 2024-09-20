bdc0fc1c78bbccaa857cf6611663cdedc21604d2
entity12208 <116198380+entity12208@users.noreply.github.com>
2024-09-19 18:53:57 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,75 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/Level.hpp>
+#include <fstream>
+#include <string>
+#include <vector>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+class $modify(Level) {
+    std::vector<int> portalOrder;
+    bool ctrlJPressed = false;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+    void onLevelStart() override {
+        Level::onLevelStart(); // Call the base class method
+        if (isCtrlJPressed()) {
+            scanPortalOrder();
+        }
+    }
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+    void onDeath() override {
+        Level::onDeath(); // Call the base class method
+        retryLevelWithJump();
+    }
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+    bool isCtrlJPressed() {
+        return (GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState('J') & 0x8000);
+    }
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+    void scanPortalOrder() {
+        portalOrder.clear();
+        for (auto& portal : getPortals()) {
+            portalOrder.push_back(portal->getType());
+        }
+        ctrlJPressed = true;
+    }
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+    void retryLevelWithJump() {
+        if (ctrlJPressed) {
+            for (int i = 0; i < 3; ++i) {
+                jumpBeforeDeath();
+            }
+            logOutcome();
+        }
+    }
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
+    void jumpBeforeDeath() {
+        // Logic to jump before the moment of death
+        auto player = getPlayer();
+        if (player) {
+            player->jump();
+        }
+    }
 
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+    void logOutcome() {
+        std::ofstream logFile;
+        logFile.open(getLevelName() + ".txt", std::ios::app);
+        logFile << "Outcome logged\n";
+        logFile.close();
+    }
+
+    std::string getLevelName() {
+        auto level = getCurrentLevel();
+        return level ? level->getName() : "Unknown";
+    }
+
+    std::vector<Portal*> getPortals() {
+        auto level = getCurrentLevel();
+        return level ? level->getPortals() : std::vector<Portal*>{};
+    }
+
+    Player* getPlayer() {
+        auto level = getCurrentLevel();
+        return level ? level->getPlayer() : nullptr;
+    }
 };
